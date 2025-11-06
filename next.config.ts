5546365b7d671d016149a54b4ae833d3e89a9384
MM <oodollaroo.mix47@gmail.com>
2025-11-06 22:50:17 +0700

    V12.2 pro

--- next.config.ts
+++ next.config.ts
@@ -17,6 +17,11 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
+      {  
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
     ],
   },
 };
